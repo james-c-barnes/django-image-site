33322dfc6c0798f18513c3a0ffbd6e7aaf31605a
James Barnes <james_c_barnes@yahoo.com>
2016-12-17 04:25:48 +0000

    Added admin and Image model.

--- images/models.py
+++ images/models.py
@@ -1,5 +1,16 @@
 from __future__ import unicode_literals
 
 from django.db import models
+from django.utils.encoding import python_2_unicode_compatible
 
-# Create your models here.
+@python_2_unicode_compatible
+class Image(models.Model):
+    filename = models.CharField(max_length=128)
+    filetype = models.CharField(max_length=128)
+    height = models.IntegerField(default=0)
+    width = models.IntegerField(default=0)
+    created_date = models.DateTimeField('date created')
+    updated_date = models.DateTimeField('date published')
+
+    def __str__(self):
+        return "{}.{}".format(self.filename, self.filetype)
