516703a79acd90fe78236a0c5bc6bf25f8368b32
James Barnes <james_c_barnes@yahoo.com>
2016-12-17 17:43:28 +0000

    Started using Django imagefield.

--- images/models.py
+++ images/models.py
@@ -5,11 +5,7 @@ from django.utils.encoding import python_2_unicode_compatible
 
 @python_2_unicode_compatible
 class Image(models.Model):
-    name = models.CharField(max_length=128, default='')
-    filename = models.CharField(max_length=128)
-    filetype = models.CharField(max_length=128)
-    height = models.IntegerField(default=0)
-    width = models.IntegerField(default=0)
+    image = models.ImageField(upload_to='saved_images', default=None)
     created_date = models.DateTimeField('date created')
     updated_date = models.DateTimeField('date published')
 
